3512b128219e2cf7921450da598316483df59d64
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-04-14 15:40:38 +0900

    first commit

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        heightTextField.placeholder = "身長を入力してください（cm）"
+        weightTextField.placeholder = "体重を入力してください（kg）"
     }
 
-
+    @IBAction func calcAction(_ sender: Any) {
+        let doubleH = Double(heightTextField.text!)
+        let doubleW = Double(weightTextField.text!)
+        
+        bmiLabel.text = calculation(height: doubleH!, weight: doubleW!)
+    }
+    
+    func calculation(height: Double, weight: Double) -> String {
+        let h = height/100
+        let w = weight
+        var result = w / (h * h)
+        result = floor(result * 10) / 10
+        
+        return result.description
+    }
+    
 }
 
